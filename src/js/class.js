b550502a9091e0c5c52214f76d39b7be3e29544a
Syl <s.valienne@yahoo.fr>
2015-03-23 22:54:34 +0100

    updated structure

---
+++ src/js/class.js
@@ -0,0 +1,11 @@
+var Tab = (function (hey) {
+    var self = {};
+
+    self.init = function (options) {
+        return options;
+    };
+
+    return self;
+})();
+
+module.exports = Tab;
---
+++ test/test.js
@@ -0,0 +1,21 @@
+var assert = require("assert");
+var Tab = require("../src/js/class.js");
+
+console.log(Tab.init('toto'));
+console.log(Tab.init('haha'));
+// var newtab = Tab('yolo');
+//
+// describe('Array', function(){
+//   describe('#indexOf()', function(){
+//     it('should return -1 when the value is not present', function(){
+//       assert.equal(-1, [1,2,3].indexOf(5));
+//       assert.equal(-1, [1,2,3].indexOf(0));
+//       });
+//   });
+// });
+//
+// describe('tab', function() {
+//     it('should return -1 when the value is not present', function(){
+//       assert.equal(newtab.log(), 'toto');
+//   });
+// });
