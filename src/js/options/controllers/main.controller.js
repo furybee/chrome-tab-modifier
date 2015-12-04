f288ad194da0b79bbb1a56ecbbeb34a1d5dcbdae
Syl <s.valienne@yahoo.fr>
2015-12-04 09:26:29 +0100

    updated toggleAside

--- src/js/options/controllers/main.controller.js
+++ src/js/options/controllers/main.controller.js
@@ -1,8 +1,7 @@
 app.controller('MainController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
 
-    $scope.toggleAside = toggleAside;
-
-    function toggleAside() {
+    $scope.toggleAside = function () {
         $mdSidenav('aside-left').toggle();
-    }
+    };
+
 }]);
