593857888a54d48bb2d410d77e90eb88b00c0f31
MartinLichtblau <martin.lichtblau@gmail.com>
2019-05-17 17:44:47 +0200

    synced dist and src files, namely background.js and content.js

--- dist/js/background.js
+++ dist/js/background.js
@@ -6,13 +6,13 @@ let options_url = chrome.extension.getURL('html/options.html'), openOptionsPage,
 // Functions
 
 openOptionsPage = function (hash) {
-    chrome.tabs.query({ url: options_url }, function (tabs) {
+    chrome.tabs.query({url: options_url}, function (tabs) {
         if (tabs.length > 0) {
-            chrome.tabs.update(tabs[0].id, { active: true, highlighted: true }, function (current_tab) {
-                chrome.windows.update(current_tab.windowId, { focused: true });
+            chrome.tabs.update(tabs[0].id, {active: true, highlighted: true}, function (current_tab) {
+                chrome.windows.update(current_tab.windowId, {focused: true});
             });
         } else {
-            chrome.tabs.create({ url: (hash !== undefined) ? options_url + '#' + hash : options_url });
+            chrome.tabs.create({url: (hash !== undefined) ? options_url + '#' + hash : options_url});
         }
     });
 };
@@ -23,82 +23,81 @@ getStorage = function (callback) {
     });
 };
 
-function triggerContentScript(tabId, rule) {
-		chrome.tabs.sendMessage(tabId, 'isContentScriptInjected', function (response) {
-				if (chrome.runtime.lastError || !response) {
-						// console.log("Content.js NOT injected yet in tabId "+tabId+". So inject now!");
-						chrome.tabs.executeScript(tabId, {file: 'js/content.js'}, function(results) {
-								if(!chrome.runtime.lastError){ // read possible error "Unchecked runtime.lastError: The tab was closed." so it doesn't get thrown
-									chrome.tabs.sendMessage(tabId, {runRule: rule}); // wait until the script is injected and just then send run message
-								}
-						});
-				} else {
-						// console.log("Content.js is already injected, so run with pages rule now.");
-						chrome.tabs.sendMessage(tabId, {runRule: rule});
-				}
-		});
+function getRule(url, callback) {
+    var thisRule = null;
+    getStorage(function (tab_modifier) {
+        if (tab_modifier === undefined) {
+            return callback(thisRule);
+        }
+        for (var i = 0; i < tab_modifier.rules.length; i++) {
+            var listedRule = tab_modifier.rules[i];
+            if (listedRule.detection === undefined || listedRule.detection === 'CONTAINS') {
+                if (url.indexOf(listedRule.url_fragment) !== -1) {
+                    thisRule = listedRule;
+                    break;
+                }
+            } else {
+                switch (listedRule.detection) {
+                    case 'STARTS':
+                        if (url.startsWith(listedRule.url_fragment) === true) {
+                            thisRule = listedRule;
+                            break;
+                        }
+                        break;
+                    case 'ENDS':
+                        if (url.endsWith(listedRule.url_fragment) === true) {
+                            thisRule = listedRule;
+                            break;
+                        }
+                        break;
+                    case 'REGEXP':
+                        var regexp = new RegExp(listedRule.url_fragment);
+
+                        if (regexp.test(url) === true) {
+                            thisRule = listedRule;
+                            break;
+                        }
+                        break;
+                    case 'EXACT':
+                        if (url === listedRule.url_fragment) {
+                            thisRule = listedRule;
+                            break;
+                        }
+                        break;
+                }
+            }
+        }
+        return callback(thisRule);
+    });
 };
 
-function getRule(url, callback) {
-		var thisRule = null;
-		
-		getStorage(function (tab_modifier) {
-				if (tab_modifier === undefined) {
-						return callback(thisRule);
-				}				
-				for (var i = 0; i < tab_modifier.rules.length; i++) {
-						var listedRule = tab_modifier.rules[i];
-						if (listedRule.detection === undefined || listedRule.detection === 'CONTAINS') {
-								if (url.indexOf(listedRule.url_fragment) !== -1) {
-										thisRule = listedRule;
-										break;
-								}
-						} else {
-								switch (listedRule.detection) {
-										case 'STARTS':
-												if (url.startsWith(listedRule.url_fragment) === true) {
-														thisRule = listedRule;
-														break;
-												}
-												break;
-										case 'ENDS':
-												if (url.endsWith(listedRule.url_fragment) === true) {
-														thisRule = listedRule;
-														break;
-												}
-												break;
-										case 'REGEXP':
-												var regexp = new RegExp(listedRule.url_fragment);
-												
-												if (regexp.test(url) === true) {
-														thisRule = listedRule;
-														break;
-												}
-												break;
-										case 'EXACT':
-												if (url === listedRule.url_fragment) {
-														thisRule = listedRule;
-														break;
-												}
-												break;
-								}
-						}
-				};		
-				return callback(thisRule);
-		});
+function triggerContentScript(tabId, rule) {
+    chrome.tabs.sendMessage(tabId, 'isContentScriptInjected', function (response) {
+        if (chrome.runtime.lastError || !response) {
+            // console.log("Content.js NOT injected yet in tabId "+tabId+". So inject now!");
+            chrome.tabs.executeScript(tabId, {file: 'js/content.js'}, function (results) {
+                if (!chrome.runtime.lastError) { // read possible error "Unchecked runtime.lastError: The tab was closed." so it doesn't get thrown
+                    chrome.tabs.sendMessage(tabId, {runRule: rule}); // wait until the script is injected and just then send run message
+                }
+            });
+        } else {
+            // console.log("Content.js is already injected, so run with pages rule now.");
+            chrome.tabs.sendMessage(tabId, {runRule: rule});
+        }
+    });
 };
 
 // --------------------------------------------------------------------------------------------------------
 // Events
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-		if (changeInfo.status === 'complete') {
-				getRule(tab.url, function(rule) {
-						if (rule !== null) {
-								triggerContentScript(tab.id, rule);
-						}
-				});
-		}
+    if (changeInfo.status === 'complete') {
+        getRule(tab.url, function (rule) {
+            if (rule !== null) {
+                triggerContentScript(tab.id, rule);
+            }
+        });
+    }
 });
 
 chrome.runtime.onMessage.addListener(function (message, sender) {
@@ -108,21 +107,21 @@ chrome.runtime.onMessage.addListener(function (message, sender) {
                 if (current_tab === undefined) {
                     return;
                 }
-                
+
                 let tab, tab_id;
-                
+
                 chrome.tabs.query({}, function (tabs) {
                     for (let i = 0; i < tabs.length; i++) {
                         tab = tabs[i];
-                        
+
                         if (tab.url.indexOf(message.url_fragment) !== -1 && tab.id !== current_tab.id) {
                             tab_id = tab.id;
-                            
+
                             chrome.tabs.executeScript(current_tab.id, {
                                 code: 'window.onbeforeunload = null;'
                             }, function () {
                                 chrome.tabs.remove(current_tab.id);
-                                
+
                                 chrome.tabs.update(tab_id, {
                                     url: current_tab.url,
                                     highlighted: true
@@ -151,13 +150,13 @@ chrome.browserAction.onClicked.addListener(function () {
 });
 
 chrome.runtime.onInstalled.addListener(function (details) {
-		// create on intall and add listener every time; see: https://stackoverflow.com/questions/26245888/adding-context-menu-item-on-a-non-persistent-background-script
-		chrome.contextMenus.create({
-				id: 'rename-tab',
-				title: 'Rename Tab',
-				contexts: ['all']
-		});
-		
+    // create only once on install, but add listener every time on reload; see: https://stackoverflow.com/questions/26245888/adding-context-menu-item-on-a-non-persistent-background-script
+    chrome.contextMenus.create({
+        id: 'rename-tab',
+        title: 'Rename Tab',
+        contexts: ['all']
+    });
+
     switch (details.reason) {
         case 'install':
             openOptionsPage('install');
@@ -167,7 +166,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
                 if (tab_modifier === undefined || tab_modifier.settings === undefined) {
                     return;
                 }
-                
+
                 if (tab_modifier.settings !== undefined && tab_modifier.settings.enable_new_version_notification === true && details.previousVersion !== chrome.runtime.getManifest().version) {
                     openOptionsPage('update/' + chrome.runtime.getManifest().version);
                 }
@@ -179,10 +178,10 @@ chrome.runtime.onInstalled.addListener(function (details) {
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === 'rename-tab') {
         let title = prompt('Enter the new title, a Tab rule will be automatically created for you based on current URL');
-				if (title === null) { 
-						return; // user pressed cancel
-				}
-        
+        if (title === null) {
+            return; // user pressed cancel
+        }
+
         getStorage(function (tab_modifier) {
             if (tab_modifier === undefined) {
                 tab_modifier = {
@@ -192,7 +191,7 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
                     rules: []
                 };
             }
-            
+
             let rule = {
                 name: 'Rule created from right-click (' + tab.url.replace(/(^\w+:|^)\/\//, '').substring(0, 15) + '...)',
                 detection: 'CONTAINS',
@@ -208,11 +207,11 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
                     url_matcher: null
                 }
             };
-            
+
             tab_modifier.rules.push(rule);
-            
-            chrome.storage.local.set({ tab_modifier: tab_modifier });
-            
+
+            chrome.storage.local.set({tab_modifier: tab_modifier});
+
             chrome.tabs.reload(tab.id);
         });
     }
--- dist/js/content.js
+++ dist/js/content.js
@@ -1,201 +1,200 @@
 var w = window, rule = null, processPage, tryCount, retryTimer;
-		
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-		if (request === "isContentScriptInjected") {
-				sendResponse({contentScriptInjected: 'true'});
-		} else if (request.runRule) {
-				sendResponse({runRule: 'true'});
-				rule = request.runRule;
-				// reset tryCount since it's the first try, and clear any previous retryTimer that's already waiting
-				clearTimeout(retryTimer);
-				tryCount = 0; 
-				// Even on first try use timer to delay function call of processPage, so that rare but possible fast onUpdate events won't rapidly call it again
-				retryTimer = setTimeout(processPage, 1000);
-		}
-});		
-    
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request === "isContentScriptInjected") {
+        sendResponse({contentScriptInjected: 'true'});
+    } else if (request.runRule) {
+        rule = request.runRule;
+        // reset tryCount since it's the first try, and clear any previous retryTimer that's already waiting
+        clearTimeout(retryTimer);
+        tryCount = 0;
+        // Even on first try use timer to delay call of processPage, so that fast onUpdate events won't rapidly call it again
+        retryTimer = setTimeout(processPage, 1000);
+    }
+});
+
 processPage = function () {
-		console.log("run processPage()");
-		
-		var getTextBySelector, updateTitle, processTitle, processIcon;
-		
-		/**
-		 * Returns the text related to the given CSS selector
-		 * @param selector
-		 * @returns {string}
-		 */
-		getTextBySelector = function (selector) {
-				var el = document.querySelector(selector), value = '';
-				
-				if (el !== null) {
-						el = el.childNodes[0];
-						
-						if (el.tagName === 'input') {
-								value = el.value;
-						} else if (el.tagName === 'select') {
-								value = el.options[el.selectedIndex].text;
-						} else {
-								value = el.innerText || el.textContent;
-						}
-				}
-				
-				return value.trim();
-		};
-		
-		/**
-		 * Update title string by replacing given tag by value
-		 * @param title
-		 * @param tag
-		 * @param value
-		 * @returns {*}
-		 */
-		updateTitle = function (title, tag, value) {
-				if (value === '') {
-						return title;
-				}
-				
-				return title.replace(tag, value);
-		};
-		
-		/**
-		 * Process new title depending on current URL & current title
-		 * @param current_url
-		 * @param current_title
-		 * @returns {*}
-		 */
-		processTitle = function (current_url, current_title) {
-				var title = rule.tab.title, matches = title.match(/\{([^}]+)}/g), i;
-				var defaultTitle = title;
-				
-				// Handle curly braces tags inside title
-				if (matches !== null) {
-						var selector, text;
-						
-						for (i = 0; i < matches.length; i++) {
-								selector = matches[i].substring(1, matches[i].length - 1);
-								text     = getTextBySelector(selector);
-								title    = updateTitle(title, matches[i], text);
-						}
-						
-						if (selector && !text) { // selector is defined, but nothing was found, so it failed
-								console.log("FAILED to extract text from selector");									
-								return null;
-						}
-				}
-				
-				// Handle title_matcher
-				if (rule.tab.title_matcher !== null) {
-						try {
-								matches = current_title.match(new RegExp(rule.tab.title_matcher), 'g');
-								
-								if (matches !== null) {
-										for (i = 0; i < matches.length; i++) {
-												title = updateTitle(title, '@' + i, matches[i]);
-										}
-								}
-						} catch (e) {
-								console.log(e);
-						}
-				}
-				
-				// Handle url_matcher
-				if (rule.tab.url_matcher !== null) {
-						try {
-								matches = current_url.match(new RegExp(rule.tab.url_matcher), 'g');
-								
-								if (matches !== null) {
-										for (i = 0; i < matches.length; i++) {
-												title = updateTitle(title, '$' + i, matches[i]);
-										}
-								}
-						} catch (e) {
-								console.log(e);
-						}
-				}
-				
-				return title;
-		};
-		
-		/**
-		 * Remove existing favicon(s) and create a new one
-		 * @param new_icon
-		 * @returns {boolean}
-		 */
-		processIcon = function (new_icon) {
-				var el, icon, link;
-				
-				el = document.querySelectorAll('head link[rel*="icon"]');
-				
-				// Remove existing favicons
-				Array.prototype.forEach.call(el, function (node) {
-						node.parentNode.removeChild(node);
-				});
-				
-				// Set preconfigured or custom (http|https|data) icon
-				icon = (/^(https?|data):/.test(new_icon) === true) ? new_icon : chrome.extension.getURL('/img/' + new_icon);
-				
-				// Create new favicon
-				link      = document.createElement('link');
-				link.type = 'image/x-icon';
-				link.rel  = 'icon';
-				link.href = icon;
-				
-				document.getElementsByTagName('head')[0].appendChild(link);
-				
-				return true;
-		};
-		
-		// Set title
-		if (rule.tab.title !== null) {
-				if (document.title !== null) {
-						console.log("OLD title: "+document.title);
-						var newTitle = processTitle(location.href, document.title);
-						if (newTitle == null) { // means, it couldn't extract text from DOM selector
-								retry();
-								return; // stop processPage() for now at this point
-						} else {
-								console.log("NEW Title: "+newTitle);
-						}
-						document.title = newTitle;
-				}
-		}
-		
-		// Pin the tab
-		if (rule.tab.pinned === true) {
-				chrome.runtime.sendMessage({ action: 'setPinned' });
-		}
-		
-		// Set new icon
-		if (rule.tab.icon !== null) {
-				processIcon(rule.tab.icon);
-		}
-		
-		// Protect the tab
-		if (rule.tab.protected === true) {
-				w.onbeforeunload = function () {
-						return '';
-				};
-		}
-		
-		// Keep this tab unique
-		if (rule.tab.unique === true) {
-				chrome.runtime.sendMessage({
-						action: 'setUnique',
-						url_fragment: rule.url_fragment
-				});
-		}
-		
-		// Mute the tab
-		if (rule.tab.muted === true) {
-				chrome.runtime.sendMessage({ action: 'setMuted' });
-		}
-		console.log("finished processPage()");
-};   
+    // console.log("run processPage()");
+
+    var getTextBySelector, updateTitle, processTitle, processIcon;
+
+    /**
+     * Returns the text related to the given CSS selector
+     * @param selector
+     * @returns {string}
+     */
+    getTextBySelector = function (selector) {
+        var el = document.querySelector(selector), value = '';
+
+        if (el !== null) {
+            el = el.childNodes[0];
+
+            if (el.tagName === 'input') {
+                value = el.value;
+            } else if (el.tagName === 'select') {
+                value = el.options[el.selectedIndex].text;
+            } else {
+                value = el.innerText || el.textContent;
+            }
+        }
+
+        return value.trim();
+    };
+
+    /**
+     * Update title string by replacing given tag by value
+     * @param title
+     * @param tag
+     * @param value
+     * @returns {*}
+     */
+    updateTitle = function (title, tag, value) {
+        if (value === '') {
+            return title;
+        }
+
+        return title.replace(tag, value);
+    };
+
+    /**
+     * Process new title depending on current URL & current title
+     * @param current_url
+     * @param current_title
+     * @returns {*}
+     */
+    processTitle = function (current_url, current_title) {
+        var title = rule.tab.title, matches = title.match(/\{([^}]+)}/g), i;
+        var defaultTitle = title;
+
+        // Handle curly braces tags inside title
+        if (matches !== null) {
+            var selector, text;
+
+            for (i = 0; i < matches.length; i++) {
+                selector = matches[i].substring(1, matches[i].length - 1);
+                text = getTextBySelector(selector);
+                title = updateTitle(title, matches[i], text);
+            }
+
+            if (selector && !text) { // selector is defined, but nothing was found, so it failed
+                // console.log("FAILED to extract text from selector");
+                return null;
+            }
+        }
+
+        // Handle title_matcher
+        if (rule.tab.title_matcher !== null) {
+            try {
+                matches = current_title.match(new RegExp(rule.tab.title_matcher), 'g');
+
+                if (matches !== null) {
+                    for (i = 0; i < matches.length; i++) {
+                        title = updateTitle(title, '@' + i, matches[i]);
+                    }
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        // Handle url_matcher
+        if (rule.tab.url_matcher !== null) {
+            try {
+                matches = current_url.match(new RegExp(rule.tab.url_matcher), 'g');
+
+                if (matches !== null) {
+                    for (i = 0; i < matches.length; i++) {
+                        title = updateTitle(title, '$' + i, matches[i]);
+                    }
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        return title;
+    };
+
+    /**
+     * Remove existing favicon(s) and create a new one
+     * @param new_icon
+     * @returns {boolean}
+     */
+    processIcon = function (new_icon) {
+        var el, icon, link;
+
+        el = document.querySelectorAll('head link[rel*="icon"]');
+
+        // Remove existing favicons
+        Array.prototype.forEach.call(el, function (node) {
+            node.parentNode.removeChild(node);
+        });
+
+        // Set preconfigured or custom (http|https|data) icon
+        icon = (/^(https?|data):/.test(new_icon) === true) ? new_icon : chrome.extension.getURL('/img/' + new_icon);
+
+        // Create new favicon
+        link = document.createElement('link');
+        link.type = 'image/x-icon';
+        link.rel = 'icon';
+        link.href = icon;
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+
+        return true;
+    };
+
+    // Set title
+    if (rule.tab.title !== null) {
+        if (document.title !== null) {
+            // console.log("OLD title: " + document.title);
+            var newTitle = processTitle(location.href, document.title);
+            if (newTitle == null) { // means, it couldn't extract text from DOM selector
+                retry();
+                return; // stop this try of processPage()
+            } else {
+                // console.log("NEW Title: " + newTitle);
+            }
+            document.title = newTitle;
+        }
+    }
+
+    // Pin the tab
+    if (rule.tab.pinned === true) {
+        chrome.runtime.sendMessage({action: 'setPinned'});
+    }
+
+    // Set new icon
+    if (rule.tab.icon !== null) {
+        processIcon(rule.tab.icon);
+    }
+
+    // Protect the tab
+    if (rule.tab.protected === true) {
+        w.onbeforeunload = function () {
+            return '';
+        };
+    }
+
+    // Keep this tab unique
+    if (rule.tab.unique === true) {
+        chrome.runtime.sendMessage({
+            action: 'setUnique',
+            url_fragment: rule.url_fragment
+        });
+    }
+
+    // Mute the tab
+    if (rule.tab.muted === true) {
+        chrome.runtime.sendMessage({action: 'setMuted'});
+    }
+    // console.log("finished processPage()");
+};
 
 function retry() {
-		tryCount = tryCount+1;
-		console.log("RETRY in "+tryCount+" seconds");
-		if (tryCount <= 50) { // just so it doesn't try forever. Could be made configurable.
-			retryTimer = setTimeout(processPage, tryCount*1000);
-		}
-} 
+    tryCount = tryCount + 1;
+    // console.log("RETRY in " + tryCount + " seconds");
+    if (tryCount <= 50) { // just so it doesn't try forever. Could be customizable.
+        retryTimer = setTimeout(processPage, tryCount * 1000);
+    }
+}
