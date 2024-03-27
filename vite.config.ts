608f0011b151cde7387de9a9db11bbd27a5b5724
sebastienfontaine <hello@sebastienfontaine.me>
2024-03-27 11:18:20 +0100

    fix: ws error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { crx, ManifestV3Export } from '@crxjs/vite-plugin'
 import manifest from './manifest.json' assert { type: 'json' }
 
 export default defineConfig({
+  server: {
+    strictPort: true,
+    port: 5173,
+    hmr: {
+      clientPort: 5173
+    }
+  },
   plugins: [
     vue(),
     crx({ manifest: manifest as unknown as ManifestV3Export }),
