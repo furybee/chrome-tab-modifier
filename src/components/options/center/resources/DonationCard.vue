62bac651ca8dd45fb02261edd4a45980ad78cbbe
sebastienfontaine <hello@sebastienfontaine.me>
2024-04-06 21:05:21 +0200

    feat: add donation buttons

---
+++ src/components/options/center/resources/DonationCard.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<a class="group" :href="props.link" target="_blank">
+			<div class="card bg-base-300 group-hover:bg-base-200 shadow-xl p-4 h-full flex items-center">
+				<div
+					class="bg-gray-300 rounded-full p-1 h-[30px] w-[30px] flex items-center justify-center mb-2"
+				>
+					<slot name="icon"></slot>
+				</div>
+				{{ props.name }}
+			</div>
+		</a>
+	</div>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+	link: string;
+	name: string;
+}>();
+</script>
+
+<style lang="scss" scoped></style>
