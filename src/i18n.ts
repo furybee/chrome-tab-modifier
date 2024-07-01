c2a4e0d0603eb88852c8483b175ee9516a7ce6bc
sebastienfontaine <hello@sebastienfontaine.me>
2024-07-01 23:47:40 +0200

    fix: tests

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,7 @@ import { createI18n } from 'vue-i18n';
 const i18n = createI18n({
 	legacy: false,
 	globalInjection: true,
-	locale: chrome.i18n.getUILanguage() || 'en',
+	locale: chrome.i18n?.getUILanguage() || 'en',
 	fallbackLocale: 'en',
 	messages: {
 		// Messages initialement vides, ils seront chargés dynamiquement
